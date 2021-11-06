1ffba28b012f75e52e77d7e28955c468c38ae0d1
Chetan Satpute <chetan.satpute2002@gmail.com>
2021-11-07 00:39:05 +0530

    Updated Device Frame

--- src/components/Smartphone/smartphone.tsx
+++ src/components/Smartphone/smartphone.tsx
@@ -1,10 +1,18 @@
 import React, { FC } from "react";
-import './style.css';
+import "./style.css";
 
 const Smartphone: FC = ({ children }) => {
   return (
-    <div className="smartphone">
-      <div className="content">{children}</div>
+    <div className="md-iphone-5 md-black-device md-glare">
+      <div className="md-body">
+        <div className="md-buttons"></div>
+        <div className="md-front-camera"></div>
+        <div className="md-top-speaker"></div>
+
+        <div className="md-screen">{children}</div>
+
+        <button className="md-home-button"></button>
+      </div>
     </div>
   );
 };
